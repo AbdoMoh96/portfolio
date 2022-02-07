8421911a4db7ec970c46607cb0de8bff7a20ba2d
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2022-02-07 23:17:38 +0200

    fresh final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('temp/style.css')}}">
-    <title>Document</title>
+    <title>Abdelrahman Mohammed</title>
 </head>
 <body>
 <div class="vertical-container background" id="main">
@@ -14,6 +14,7 @@
         <div class="container">
             <h2 class="title">Comming Soon</h2>
             <p>Something will appear here</p>
+            <a href="https://github.com/AbdoMoh96" target="_blank">GitHub</a>
         </div>
     </div>
 </div>
